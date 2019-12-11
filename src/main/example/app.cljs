88c7dc5dc360203334f49b6b377ddf903b36456a
Neil Okamoto <neil.okamoto@gmail.com>
2019-12-10 17:17:50 -0800

    set up Tester component

--- src/main/example/app.cljs
+++ src/main/example/app.cljs
@@ -1,5 +1,6 @@
 (ns example.app
   (:require
+   ["cavy" :as cavy :refer (Tester TestHookStore useCavy)]
    ["expo" :as ex]
    ["react-native" :as rn]
    ["react" :as react]
@@ -14,6 +15,8 @@
 ;; must use path relative to :output-dir
 
 (defonce splash-img (js/require "../assets/shadow-cljs.png"))
+
+(def test-hook-store (TestHookStore.))
 
 (def styles
   ^js (-> {:container
@@ -47,13 +50,17 @@
 (defn root []
   (let [counter (rf/subscribe [:get-counter])]
     (fn []
-      [:> rn/View {:style (.-container styles)}
-       [:> rn/Text {:style (.-title styles)} "Clicked: " @counter]
-       [:> rn/TouchableOpacity {:style (.-button styles)
-                                :on-press #(rf/dispatch [:inc-counter])}
-        [:> rn/Text {:style (.-buttonText styles)} "Click me, I'll count"]]
-       [:> rn/Image {:source splash-img :style {:width 200 :height 200}}]
-       [:> rn/Text {:style (.-label styles)} "Using: shadow-cljs+expo+reagent+re-frame"]])))
+      ;;(let [generateTestHook (useCavy)])
+      [:> Tester {:specs [] :store test-hook-store}
+       [:> rn/View {:style (.-container styles)}
+        [:> rn/Text {:style (.-title styles)} "Clicked: " @counter]
+        [:> rn/TouchableOpacity {:style    (.-button styles)
+                                 :on-press #(rf/dispatch [:inc-counter])}
+         [:> rn/Text {:style (.-buttonText styles)} "Click me, I'll count"]]
+        [:> rn/Image {:source splash-img :style {:width 200 :height 200}
+                      ;;:ref (generateTestHook "LogoImage")
+                      }]
+        [:> rn/Text {:style (.-label styles)} "Using: shadow-cljs+expo+reagent+re-frame"]]])))
 
 (defn start
   {:dev/after-load true}
